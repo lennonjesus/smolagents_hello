4118d6c604887c9a4c8c83b70bb7a330e34b5ff7
Lennon Jesus <lennon.jesus@gmail.com>
2024-12-31 20:04:08 -0300

    chore: Versão inicial

---
+++ app.py
@@ -0,0 +1,14 @@
+from smolagents import CodeAgent, DuckDuckGoSearchTool, HfApiModel
+from huggingface_hub import login
+
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+login(os.getenv("HUGGINGFACE_API_KEY"))
+
+agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
+
+# agent.run("Quantos tempo levaria para um carro atual de fórmula 1 percorrer toda a extensão da Ponte Rio-Niterói, respeitando seus limites de velocidade?")
+agent.run("Quanto tempo de viagem de avião entre GIG e SCL?")
