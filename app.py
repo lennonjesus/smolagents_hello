10cabc974b0cd27b031ab2795716e8410583eb07
Lennon Jesus <lennon.jesus@gmail.com>
2024-12-31 20:04:54 -0300

    feat: Altera query

--- app.py
+++ app.py
@@ -10,5 +10,4 @@ login(os.getenv("HUGGINGFACE_API_KEY"))
 
 agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=HfApiModel())
 
-# agent.run("Quantos tempo levaria para um carro atual de fórmula 1 percorrer toda a extensão da Ponte Rio-Niterói, respeitando seus limites de velocidade?")
-agent.run("Quanto tempo de viagem de avião entre GIG e SCL?")
+agent.run("Quantos tempo levaria para um carro atual de fórmula 1 percorrer toda a extensão da Ponte Rio-Niterói, respeitando seus limites de velocidade?")
